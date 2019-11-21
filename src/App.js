7f07a2f517f272ce27319d13f10ed8221ffd896e
marwinburesch <marwin@posteo.de>
2019-11-21 12:29:52 +0100

    Add Title component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import GlobalStyles from "./utils/GlobalStyles";
 import { ThemeProvider } from "emotion-theming";
 import defaultTheme from "./themes/defaultTheme";
 import Title from "./components/Title";
-// import styled from "@emotion/styled";
 
 function App() {
   return (
---
+++ src/components/Title.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+const Title = styled.h1`
+  text-shadow: 4px 4px 0 ${props => props.theme.darklining};
+  font-size: 3rem;
+`;
+
+export default Title;
