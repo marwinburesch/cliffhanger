b1862c40d82dafc7cd2fb045d4d21a8e5e26b86a
marwinburesch <marwin@posteo.de>
2019-11-21 12:13:40 +0100

    remove higher order function for theming
    add useTheme

--- src/utils/GlobalStyles.js
+++ src/utils/GlobalStyles.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Global, css } from "@emotion/core";
-import { withTheme } from "emotion-theming";
+import { useTheme } from "emotion-theming";
 
-const GlobalStyles = withTheme(({ theme }) => (
-  <Global
-    styles={css`
-      *,
-      *::before,
-      *::after {
-        box-sizing: border-box;
-      }
-      body {
-        font-family: "Raleway", sans-serif;
-        background: ${theme.primary};
-      }
-      h1 {
-        font-family: "Abril Fatface", cursive;
-        color: ${theme.primarytext};
-      }
-    `}
-  />
-));
-
-export default GlobalStyles;
+export default function GlobalStyles() {
+  const theme = useTheme();
+  return (
+    <Global
+      styles={css`
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+        body {
+          font-family: "Raleway", sans-serif;
+          background: ${theme.primary};
+        }
+        h1 {
+          font-family: "Abril Fatface", cursive;
+          color: ${theme.primarytext};
+        }
+      `}
+    />
+  );
+}
