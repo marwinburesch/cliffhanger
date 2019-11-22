b36c012dd189e4d77db6c4f877675df83916e525
marwinburesch <marwin@posteo.de>
2019-11-22 10:57:49 +0100

    Move API key
    dont do this at home

--- src/api/shows.js
+++ src/api/shows.js
@@ -1,6 +1,6 @@
 export async function getDiscoverShows() {
   const response = await fetch(
-    `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}`
+    `https://api.themoviedb.org/3/discover/tv?api_key=4a21646ec02d2f33481ef1051b8447a6`
   );
   const data = await response.json();
   return data.results;
